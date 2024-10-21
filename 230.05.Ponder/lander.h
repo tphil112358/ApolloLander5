d6bdbaa7b19b365e6b1eef649ce9dd7517ad08ee
Jackson Clarke <104281508+jacksonclarke@users.noreply.github.com>
2024-10-21 11:37:24 -0600

    Update lander.h

--- 230.05.Ponder/lander.h
+++ 230.05.Ponder/lander.h
@@ -15,7 +15,7 @@
 #include "uiDraw.h"    // for DRAW* and RANDOM
 #include "angle.h"     // for ANGLE
 
-enum Status {PLAYING, SAFE, DEAD};
+enum Status { PLAYING, SAFE, DEAD };
 
 class TestLander;
 
@@ -26,87 +26,87 @@ class TestLander;
 class Lander
 {
    friend TestLander;
- 
+
 public:
-  Lander(const Position & posUpperRight) : status(PLAYING), fuel(1000.0) {}
+   Lander(const Position& posUpperRight) :pos(100,300), status(PLAYING), fuel(1000.0) {}
 
-  // reset the lander and its position
-  void reset(const Position& posUpperRight);
+   // reset the lander and its position
+   void reset(const Position& posUpperRight);
 
-  // get the status of the lander
-  bool isDead() const
-  {
-     if (status == DEAD)
-        return true;
-     else
-        return false;
-  }
+   // get the status of the lander
+   bool isDead() const
+   {
+      if (status == DEAD)
+         return true;
+      else
+         return false;
+   }
 
-  bool isLanded() const
-  {
-     if (status == SAFE)
-        return true;
-     else
-        return false;
-  }
+   bool isLanded() const
+   {
+      if (status == SAFE)
+         return true;
+      else
+         return false;
+   }
 
-  bool isFlying() const
-  {
-     if (status == PLAYING)
-        return true;
-     else
-        return false;
-  }
+   bool isFlying() const
+   {
+      if (status == PLAYING)
+         return true;
+      else
+         return false;
+   }
 
-  Position getPosition() const
-  {
-     return pos;
-  }
+   Position getPosition() const
+   {
+      return pos;
+   }
 
-  double getSpeed() const
-  {
-     return velocity.getSpeed();
-  }
+   double getSpeed() const
+   {
+      return velocity.getSpeed();
+   }
 
-  int getFuel() const
-  {
-     return fuel;
-  }
+   int getFuel() const
+   {
+      return fuel;
+   }
 
-  int getWidth() const
-  {
-     return 20;
-  }
+   int getWidth() const
+   {
+      return 20;
+   }
 
-  double getMaxSpeed() const
-  {
-     return 4.0;
-  }
+   double getMaxSpeed() const
+   {
+      return 4.0;
+   }
 
-  // draw the lander on the screen
-  void draw(const Thrust & thrust, ogstream & gout) const;
- 
-  // handle input
-  Acceleration input(const Thrust & thrust, double gravity);
+   // draw the lander on the screen
+   void draw(const Thrust& thrust, ogstream& gout) const;
 
-  // what happens when we coast?
-  void coast(Acceleration & acceleration, double time);
+   // handle input
+   Acceleration input(const Thrust& thrust, double gravity);
 
-  // straighten the lander and put it on the ground
-  void land()
-  {
-     angle = 0.0;
-     velocity.set(0.0, 0.0);
-     status = SAFE;
-  }
+   // what happens when we coast?
+   void coast(Acceleration& acceleration, double time);
 
-  // we are dead. Draw the lander upside down
-  void crash()
-  {
-     angle = M_PI;
-     velocity.set(M_PI, 0.0);
-     status = DEAD; // Stretch goal; insert Dark Souls 'YOU DIED' display code here
-  }
+   // straighten the lander and put it on the ground
+   void land()
+   {
+      angle.setDegrees(0.0);
+      velocity.set(0.0, 0.0);
+      status = SAFE;
+   }
+
+   // we are dead. Draw the lander upside down
+   void crash()
+   {
+      angle.setRadians(M_PI);
+      velocity.set(M_PI, 0.0);
+      status = DEAD; // Stretch goal; insert Dark Souls 'YOU DIED' display code here
+   }
 
 private:
    Status   status;      // are we dead or not?
