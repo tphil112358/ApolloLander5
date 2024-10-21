eaba44c194b6d0d0df3fe6f12937fe1237705259
Jackson Clarke <104281508+jacksonclarke@users.noreply.github.com>
2024-10-21 11:38:09 -0600

    Update thrust.h

--- 230.05.Ponder/thrust.h
+++ 230.05.Ponder/thrust.h
@@ -26,30 +26,62 @@ class Thrust
 public:
    // Thrust is initially turned off
    Thrust() : mainEngine(false), clockwise(false), counterClockwise(false) {}
-
+   
    // Get rotation in radians per second
    double rotation() const
    {
-      return 99.9;
+      double radians = 0.0; 
+      if (isClock()) 
+      {
+         radians += 0.1;
+      }
+      if (isCounter()) 
+      {
+         radians += -0.1;
+      }
+      return radians;
+      
    }
 
    // get main engine thrust in  m / s ^ 2
    double mainEngineThrust() const
    {
-      return 99.9;
+      double thrust = 45000.0 / 15103.0;
+      return thrust;
    }
 
    // reflect what is firing
-   bool isMain()    const { return true; }
-   bool isClock()   const { return true; }
-   bool isCounter() const { return true; }
+   bool isMain()    const { return mainEngine; }
+   bool isClock()   const { return clockwise; }
+   bool isCounter() const { return counterClockwise; }
 
    // set the thrusters
    void set(const Interface * pUI)
    {
-      mainEngine       = true;
-      clockwise        = true;
-      counterClockwise = true;
+      if (pUI->isRight() > 0)              // If the user is pressing right, turn on counterClockwise.
+      {
+         counterClockwise = true;
+      }
+      else                                 // If the user is not pressing right, turn off counterClockwise.
+      {
+         counterClockwise = false;
+      }
+      if (pUI->isLeft() != 0)              // If the user is pressing left, turn on clockwise.
+      {
+         clockwise = true;
+      }
+      else                                 // If the user is not pressing left, turn off clockwise.
+      {
+         clockwise = false;
+      }
+      if (pUI->isDown() != 0)              // If the user is pressing down, turn on mainEngine.
+      {
+         mainEngine = true;
+      }
+      else                                 // If the user is not pressing down, turn off mainEngine.
+      {
+         mainEngine = false;
+      }
    }
 
 private:
